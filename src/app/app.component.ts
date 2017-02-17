014e3c40db04d5daa895433e6963a5e5c18dcbfa
Benedict D <duebibenedict@gmail.com>
2017-02-17 19:18:44 +0100

    Basic random fight

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cwa works!';
+  title = 'Code Wars'; 
+  result = '';
+  highscore = 0;
+  games = 0;
+
+  fight() {
+    const randResult = Math.floor((Math.random() * 3) + 1);
+    this.games++;
+    switch(randResult) {
+      case 1:
+        this.result = 'you won!';
+        this.highscore++;
+        break;
+      case 2:
+        this.result = 'you lost';
+        this.highscore--;
+        if(this.highscore < 1) {
+          this.result = 'game over';
+          this.games = 0;
+        }
+        break;
+      default:
+        this.result = 'tie game';
+    }
+  }
 }
